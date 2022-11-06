9fa28c7c0cf712f129de9145a6f7708520337ba1
AMS006 <anassain13@gmail.com>
2022-11-06 10:35:07 +0530

    Updated

--- src/Context/Movie.context.jsx
+++ src/Context/Movie.context.jsx
@@ -2,7 +2,6 @@ import React, { createContext,useState } from 'react'
 
 export const  MovieContext =  createContext();
 
-
 function MovieProvider({children}) {
   const [movie, setMovie] = useState({
     id: 0,
